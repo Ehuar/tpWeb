f5356b87094374bbd8e585896e6e707a5661bcf9
Ehuar <nellyehua@yahoo.com>
2021-03-13 21:55:12 +0100

    creation of DnD of its functions

--- interaction.js
+++ interaction.js
@@ -1,23 +1,42 @@
-
 // La création d'un Dnd requière un canvas et un interacteur.
 // L'interacteur viendra dans un second temps donc ne vous en souciez pas au départ.
 function DnD(canvas, interactor) {
-	// Définir ici les attributs de la 'classe'
 
-	// Developper les 3 fonctions gérant les événements
+    // Définir ici les attributs de la 'classe'
+    this.xInitial = 0;
+    this.yInitial = 0;
+    this.xFinal = 0;
+    this.yFinal = 0;
 
-	// Associer les fonctions précédentes aux évènements du canvas.
-};
+    // Developper les 3 fonctions gérant les événements
+    this.pressured = function (evt) {
+
+    }
+    this.moving = function (evt) {
+
+    }
+    this.released = function (evt) {
+
+    }
+
+    // Associer les fonctions précédentes aux évènements du canvas.
+   /* canvas.addEventListener("mousedown",this.pressure());
+    canvas.addEventListener("mousemove",this.move());
+    canvas.addEventListener("mouseup",this.release());*/
+    canvas.pressured(interactor);
+    canvas.moving(interactor);
+    canvas.released(interactor);
+}
 
 
 // Place le point de l'événement evt relativement à la position du canvas.
 function getMousePosition(canvas, evt) {
-  var rect = canvas.getBoundingClientRect();
-  return {
-    x: evt.clientX - rect.left,
-    y: evt.clientY - rect.top
-  };
-};
+    var rect = canvas.getBoundingClientRect();
+    return {
+        x: evt.clientX - rect.left,
+        y: evt.clientY - rect.top
+    };
+}
 
 
 
